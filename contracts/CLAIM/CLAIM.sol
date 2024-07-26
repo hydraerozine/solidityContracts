8de08fd760f0ef84cd1cdb9470f68cc74da2b312
Jorge Bartra <jorgebartrav@gmail.com>
2024-07-26 07:31:17 -0500

    Adding more chainfusion claim

--- contracts/CLAIM/CLAIM.sol
+++ contracts/CLAIM/CLAIM.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
-contract TokenUserBalanceList is Ownable, ReentrancyGuard {
+contract CLAIM is Ownable, ReentrancyGuard {
     IERC20 public immutable token;
 
     struct User {
